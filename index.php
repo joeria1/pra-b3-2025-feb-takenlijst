b023cc7b35df387ac4325cbb8d6f4e209b236041
joeria1 <joeri.degardeijn@gmail.com>
2025-03-14 11:32:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="nl">
-
 <head>
-    <title></title>
-    <?php require_once 'head.php'; ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pretpark Webapp</title>
+    <
 </head>
-
 <body>
-    
     <div class="container">
-
-
+        <h1>Welkom bij Pretpark Webapp</h1>
+        <img src="logo.png" alt="Pretpark Logo">
+        <p>Beheer je taken.</p>
+        <a href="tasks/index.php">Naar het takenoverzicht</a>
     </div>
-
 </body>
-
 </html>
