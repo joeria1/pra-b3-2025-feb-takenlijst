33d4586ecc279ed41ca8407d595a7a9b14a9af71
Djmaxbase <maxbaselier@hotmail.com>
2025-03-20 14:07:30 +0100

    Update taskcontroller.php

--- backend/taskcontroller.php
+++ backend/taskcontroller.php
@@ -19,11 +19,6 @@ function createTask($data)
     $stmt->execute([$title, $description, $department, $status]);
 
     header("Location: ../taken/index.php");
-    $userId = $_SESSION['user_id']; // Haal het user-id uit de sessie
-    $stmt = $pdo->prepare("INSERT INTO taken (title, description, department, status, user) VALUES (?, ?, ?, ?, ?)");
-    $stmt->execute([$title, $description, $department, $status, $userId]);
-
-    header("Location: ../taken/index.php");
     exit;
 }
 
@@ -33,11 +28,3 @@ function getTasks()
     $stmt = $pdo->query("SELECT * FROM taken WHERE status <> 'done'");
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-function getMyTasks()
-{
-    $pdo = getDatabaseConnection();
-    $userId = $_SESSION['user_id']; // Haal het user-id uit de sessie
-    $stmt = $pdo->prepare("SELECT * FROM taken WHERE user = ?");
-    $stmt->execute([$userId]);
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}w
