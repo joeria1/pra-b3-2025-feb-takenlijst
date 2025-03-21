a44cd14265cc22e71b9191615d80bb9f1be07220
joeria1 <joeri.degardeijn@gmail.com>
2025-03-21 11:05:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pretpark Webapp</title>
-    <link rel="stylesheet" href="../css/main.css">
+    
+    <link rel="stylesheet" href="css/main.css">
+
 </head>
 <body>
     <div class="container">
