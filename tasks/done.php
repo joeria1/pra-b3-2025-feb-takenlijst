ea0209ab79e4b59bf5280bdb08b03fb999dc6bc2
Djmaxbase <maxbaselier@hotmail.com>
2025-03-21 11:48:26 +0100

    Update done.php

--- tasks/done.php
+++ tasks/done.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '../../../backend/config1.php'; 
+require_once __DIR__ . '/../backend/config1.php'; 
 
 
 $sql = "SELECT title, department FROM tasks WHERE status = 'done'";
